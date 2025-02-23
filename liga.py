6f3792f6986dc82e80968f97194a0b2714b5ea88
DANIEL MOYA FUSTER <danielmoyafuster@gmail.com>
2025-02-23 12:39:09 +0100

    Add files via upload

--- liga.py
+++ liga.py
@@ -24,6 +24,7 @@ if "is_mobile" not in st.session_state:
 
 # Capturar ancho de pantalla en cada ejecución (para refinar la detección)
 st.session_state["window_width"] = st.query_params().get("width", [1024])[0]
+
 def pagina_jugadores(equipo):
     st.title(f"👕 Jugadores de {equipo}")
 
